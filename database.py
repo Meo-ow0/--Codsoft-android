1f77ebc46d1e40ca69bf06201752b4fe2b980180
Shreya Chaturvedi <shreyachaturvedishreya@gmail.com>
2026-06-28 14:53:25 +0530

    Add files via upload

--- database.py
+++ database.py
@@ -1,44 +1,24 @@
 import sqlite3
 
 def get_db():
-    conn = sqlite3.connect('todo.db')
+    conn = sqlite3.connect('alarms.db')
     conn.row_factory = sqlite3.Row
     return conn
 
-
 def init_db():
     conn = get_db()
-
-    # table for storing tasks + deadline
-    conn.execute('''CREATE TABLE IF NOT EXISTS tasks 
-                    (id INTEGER PRIMARY KEY, task TEXT, deadline TEXT)''')
-
+    conn.execute('CREATE TABLE IF NOT EXISTS alarms (id INTEGER PRIMARY KEY, time TEXT, label TEXT)')
     conn.commit()
     conn.close()
 
-
-def add_task(task, deadline):
+def add_alarm(time, label):
     conn = get_db()
-
-    # insert task into database
-    conn.execute(
-        'INSERT INTO tasks (task, deadline) VALUES (?, ?)',
-        (task, deadline)
-    )
-
+    conn.execute("INSERT INTO alarms (time, label) VALUES (?, ?)", (time, label))
     conn.commit()
     conn.close()
 
-
-def get_tasks():
+def get_alarms():
     conn = get_db()
-    tasks = conn.execute('SELECT * FROM tasks').fetchall()
+    alarms = conn.execute("SELECT id, time, label FROM alarms").fetchall()
     conn.close()
-    return tasks
-
-
-def delete_task(task_id):
-    conn = get_db()
-    conn.execute('DELETE FROM tasks WHERE id = ?', (task_id,))
-    conn.commit()
-    conn.close()
+    return alarms
